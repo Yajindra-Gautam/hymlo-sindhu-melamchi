01915b92fbb89d6b98ac5a82065d1c21f5625344
Yajindra-Gautam <yajindragtm@gmail.com>
2020-11-22 21:34:06 +0545

    First Commit

---
+++ js/owl-carosel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
